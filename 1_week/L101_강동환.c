4841580c3a3bc9abb32b441bca901dd81c258409
DongHwanGang <108117157+DongHwanGang@users.noreply.github.com>
2022-07-06 01:52:26 +0900

    file arrang

--- 1_week/L101_강동환.c
+++ 1_week/L101_강동환.c
@@ -9,7 +9,7 @@ int main (){
 	if(RowVolume > 1000 || ColumnVolume > 1000){
 		return 0;
 	}
-	//////////////////
+	
 	for(ColumnRepetition = 0; ColumnVolume > ColumnRepetition; ColumnRepetition++){
 		for(RowRepetition = 0; RowVolume > RowRepetition; RowRepetition++){
 			printf("*");
