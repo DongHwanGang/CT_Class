8a94d761ad0e1f2c23aa33d59206e8d4f552513e
DongHwanGang <108117157+DongHwanGang@users.noreply.github.com>
2022-07-12 22:23:11 +0900

    Add files via upload

--- 2_week/110_강동환.c
+++ 2_week/110_강동환.c
@@ -14,7 +14,7 @@ int cip (long long n){
 
 int* solution(long long n) {
     // 리턴할 값은 메모리를 동적 할당해주세요.
-    int* answer = (int*)malloc(4*(cip(n) + 1));
+    int* answer = (int*)malloc(4*(cip(n)));
     int count = 0;
     
     for(n; n > 0; n /= 10){
