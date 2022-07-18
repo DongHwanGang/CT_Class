39a733a1733b332fe123e33f1e4ac1f5b7996566
DongHwanGang <108117157+DongHwanGang@users.noreply.github.com>
2022-07-18 16:29:42 +0900

    Add files via upload

--- 3_week/L116_강동환.c
+++ 3_week/L116_강동환.c
@@ -3,25 +3,15 @@
 
 bool solution(const char* s) {
     bool answer = true;
-    int count, srt_len;
-    
+    int count;
+
 	for(count = 0; s[count] != '\0'; count++){
-	}
-	
-	if(count == 4 || count == 6){
-		for(count = 0; s[count] != '\0'; count++){
-			if(s[count] < '0' || s[count] > '9'){
-				answer = !answer;
-				break;
-			}
+		if(s[count] < '0' || s[count] > '9'){
+			answer = false;
 		}
 	}
 	
-	else{
-		answer = !answer;
-	}
-		
-    return answer;
+    return (count == 4 || count == 6) ? answer : false;
 }
 
 int main (){
