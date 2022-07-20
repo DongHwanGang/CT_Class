6e7650a0f82d9a2d927a75fca8c462bfc68d7620
DongHwanGang <108117157+DongHwanGang@users.noreply.github.com>
2022-07-20 23:32:41 +0900

    조건문 통합하고 간단하게 고쳐봄. 114번

--- 3_week/L114_강동환.c
+++ 3_week/L114_강동환.c
@@ -3,22 +3,20 @@
 int solution(const char* s) {
     int count, answer = 0;
     
-    if(s[0] == '+' || s[0] == '-'){
-    	for(count = 1; s[count] != '\0';count++){
-    		answer = answer*10 + (s[count] - 48);
-    	}
-    	if(s[0] == '+'){
-    		answer = +answer;
+	for(count = 0; s[count] != '\0';count++){
+		if('0' <= s[count] && '9' >= s[count]){
+			answer = answer*10 + (s[count] - '0');
 		}
-		else
-			answer = -answer;
 	}
 	
-	else
-		for(count = 0; s[count] != '\0';count++){
-    		answer = answer*10 + (s[count] - 48);
-    	}
-    
+	if(s[0] == '+'){
+		answer = +answer;
+	}
+	
+	else if(s[0] == '-'){
+		answer = -answer;
+	}
+	
     return answer;
 }
 
